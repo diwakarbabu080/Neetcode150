0dfd5fa4a01506c6eaf55702930738728083207e
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-03-23 02:41:22 +0530

    Time: 3 ms (90.10%) | Memory: 43.4 MB (69.57%) - LeetSync

--- Arrays/242-valid-anagram/valid-anagram.java
+++ Arrays/242-valid-anagram/valid-anagram.java
@@ -2,14 +2,17 @@ import java.util.*;
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-
+        if(s.length() != t.length()){
+            return false;
+        }
 
         /* approach 1
 
             1. push all values in map for s and mark their frequency 
             2. for string 2 try to decrease their frequency count while you iterate 
             3. then if we found any val in arr with values != 0 then return false else true
-        */
+            T.C : O(3N) , S.C : O(1)
+         */
         int arr[] = new int[26];
 
         for(char c : s.toCharArray()){
