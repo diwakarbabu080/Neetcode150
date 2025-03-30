21830af287ad3d8b0b984ddc19817b93aa63cab4
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-03-31 00:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (20.72%) - LeetSync

--- Blind75/268-missing-number/missing-number.java
+++ Blind75/268-missing-number/missing-number.java
@@ -1,14 +1,13 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int xor = 0;
-        for(int i  = 0;i<=n; i++){
-            xor = xor^i;
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        int xor = 0,xor2 = 0;
+        for(int i  = 0;i<n; i++){
+            xor = xor^i+1;
+            xor2 = xor2^arr[i];
         }
 
-        for(int i : nums){
-            xor = xor^i;
-        }
-        return xor;
+        
+        return xor^xor2;
     }
 }
