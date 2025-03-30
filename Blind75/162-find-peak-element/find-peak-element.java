e3b3a7ce784ce29fe766094f9ef216132e7f986c
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-03-30 14:53:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (55.68%) - LeetSync

--- Blind75/162-find-peak-element/find-peak-element.java
+++ Blind75/162-find-peak-element/find-peak-element.java
@@ -1,17 +1,6 @@
 class Solution {
     public int findPeakElement(int[] arr) {
-    //    int s = 0, e = arr.length - 1;
-
-    //     while (s < e) {
-    //         int mid = s + (e - s) / 2;
-
-    //         if (arr[mid] > arr[mid + 1]) {
-    //             e = mid; // Peak is on the left (or could be mid itself)
-    //         } else {
-    //             s = mid + 1; // Peak is on the right
-    //         }
-    //     }
-    //     return s;
+    
     int s = 0;
         int e = arr.length-1;
         while(s<e){
@@ -24,8 +13,8 @@ class Solution {
                 e = mid;
             }
         }
-        return s;
+        return e;
        
     }
 }
-            // return -1; // Target not found
+           
