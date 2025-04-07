5d3cf1bea8fdee03b89b7ee9f5e11c2fa03ec8e0
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-04-07 22:59:59 +0530

    Time: 17 ms (27.32%) | Memory: 48.4 MB (22.30%) - LeetSync

---
+++ Blind75/49-group-anagrams/group-anagrams.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        /*
+        Approach using hashmap
+        1. for every string in strs do the countOfIndexes 
+        2. make this into string  
+        add if it is not already present in hashmap along with the index i
+        3. for every string in keySet() make a list and add it ans list;
+        
+        */
+
+        if (strs.length == 0)
+            return new ArrayList<>();
+        // List<List<String>> ans = new ArrayList<>();
+        // HashMap<String,List<Integer>> map = new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+
+            // int count [] = new int[26];
+
+            String s = strs[i];
+            int countArr[] = new int[26];
+            for (char ch : s.toCharArray()) {
+                int val = ch - 'a';
+                countArr[val] += 1;
+            }
+
+            // making into string
+            StringBuilder temp = new StringBuilder();
+            for (int count : countArr) {
+                temp.append(count).append('#');
+            }
+            String key = temp.toString();
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(strs[i]);
+
+        }
+        List<List<String>> ans = new ArrayList<>();
+
+        for (List<String> group : map.values()) {
+            ans.add(group);
+        }
+
+        return ans;
+
+    }
+}
