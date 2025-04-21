fbc1bcb24ea82b6b34958d455c96bbaa28c756c8
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-04-22 04:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (90.30%) - LeetSync

--- Strviers150SheetDSA/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ Strviers150SheetDSA/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,34 +1,18 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        if (nums.length == 0)
+            return 0;
 
-        int counter = 1;
-        int prev  = nums[0];
-        for(int i = 1; i<nums.length; i++){
+        int index = 1; // Points to where the next unique element should go
 
-            while(i<nums.length && nums[i] == prev){
-                nums[i] = 101;
-                i++;
-            }
-            if(i<nums.length){
-                prev =  nums[i] ;
-            }
-           
-            
-        }
-
-        int index = 1;
-        for(int i = 1; i<nums.length; i++){
-
-            if(nums[i] != 101){
-                int temp = nums[index];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
                 nums[index] = nums[i];
-                // nums[i] = temp;
                 index++;
             }
-
         }
-        // System.out.println(Arrays.toString(nums)+" "+(index)+" ");
+
         return index;
-        
+
     }
 }
