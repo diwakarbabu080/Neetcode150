6096007dc3c4e8aa3ea38df259751bb09d75c0f6
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-04-21 19:14:01 +0530

    Time: 5 ms (18.26%) | Memory: 43.4 MB (48.98%) - LeetSync

--- Strviers150SheetDSA/349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ Strviers150SheetDSA/349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -2,23 +2,21 @@ class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
 
         /* Approach 1 
-        1. Traverse in nums1 and nums2 and make respc set1 and set2;
-        2. Make a set ans while iterating in num1 and num2 check if it is present in set1 and set2 
+        1. Traverse in nums1 and make  set1 ;
+        2. now for every element in nums2 check if is present in set1 
         3. if present add it in ans set
         */
         
         HashSet<Integer> set1 = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
+        
         for(int i: nums1){
             set1.add(i);
         }
-        for(int i: nums2){
-            set2.add(i);
-        }
+        
 
          HashSet<Integer> ans = new HashSet<>();
-        for(int i:nums1){
-            if(set1.contains(i) && set2.contains(i)){
+        for(int i:nums2){
+            if(set1.contains(i)){
                 ans.add(i);
             }
         }
