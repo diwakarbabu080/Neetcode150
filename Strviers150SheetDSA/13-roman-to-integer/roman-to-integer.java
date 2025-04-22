6980bd0f24f9eb08700d45bcfd6efd0eaf223679
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-04-23 00:03:43 +0530

    Time: 5 ms (52.51%) | Memory: 45.2 MB (11.32%) - LeetSync

---
+++ Strviers150SheetDSA/13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        // int count = 0;
+        int currVal = 0;
+        int result = 0;
+        for(int i = 0; i<s.length(); i++){
+             currVal = map.get(s.charAt(i));
+           if (i < s.length() - 1) {
+                int nextVal = map.get(s.charAt(i + 1));
+
+                if (currVal < nextVal) {
+                    result -= currVal;
+                } else {
+                    result += currVal;
+                }
+            } else {
+                result += currVal; // last char
+            }
+            // count++;
+        }
+        return result;
+    }
+}
