0e813604f2b9a7486cb932dd0191c3d33390d645
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-04-23 23:57:17 +0530

    Time: 11 ms (39.03%) | Memory: 48.6 MB (59.36%) - LeetSync

---
+++ Strviers150SheetDSA/455-assign-cookies/assign-cookies.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findContentChildren(int[] greed, int[] cookies) {
+
+        Arrays.sort(greed);
+        Arrays.sort(cookies);
+
+        int first = 0;
+        int second = 0;
+        while(first<greed.length && second<cookies.length){
+
+            if(greed[first] <= cookies[second]){
+                first++;
+                second++;
+            }
+            while(second<cookies.length && first<greed.length&& cookies[second]< greed[first]){
+                second++;
+            }
+            
+        }
+        return first;
+
+        
+    }
+}
