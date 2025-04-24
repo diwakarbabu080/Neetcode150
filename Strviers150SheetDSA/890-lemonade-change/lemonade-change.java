bbe332e63df19cbe66147ca2d2568c67acfe2fef
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-04-24 08:34:17 +0530

    Time: 2 ms (94.13%) | Memory: 56 MB (55.10%) - LeetSync

--- Strviers150SheetDSA/890-lemonade-change/lemonade-change.java
+++ Strviers150SheetDSA/890-lemonade-change/lemonade-change.java
@@ -1,36 +1,23 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        
-        if(bills[0] != 5){
-            return false;
-        }
-        
-        int fives = 0, tens = 0, twenties = 0;
-        for(int i = 0; i<bills.length; i++){
-            if(bills[i] == 5){
+        int fives = 0, tens = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
                 fives++;
-            }else if(bills[i] == 10 && fives>0){
-               tens++;
-               fives--;
-            }else if(bills[i] == 20){
-                
-                if(fives >= 1 && tens> 0){
-                    fives--;
+            } else if (bill == 10) {
+                if (fives == 0) return false;
+                fives--;
+                tens++;
+            } else { // bill == 20
+                if (tens > 0 && fives > 0) {
                     tens--;
-                    twenties++;
-                }
-                else if(fives>2){
-                     
-                     fives -= 3;
-                     twenties++;
-                }else{
+                    fives--;
+                } else if (fives >= 3) {
+                    fives -= 3;
+                } else {
                     return false;
                 }
-                
-                
-            }   
-            else{
-                return false;
             }
         }
         return true;
