96a87cd4fecec0622bcddd5843953e18365f719a
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-04-28 00:16:25 +0530

    Time: 4 ms (26.89%) | Memory: 45.6 MB (86.73%) - LeetSync

--- Strviers150SheetDSA/135-candy/candy.java
+++ Strviers150SheetDSA/135-candy/candy.java
@@ -4,38 +4,34 @@ class Solution {
         int n = ratings.length;
 
         int sum = 1;
-        int peak = 1; 
+        int peak = 1;
         int count = 1;
-        int down = 0;       
-        for(int i = 1;i<n; i++){
-            if( ratings[i]>ratings[i-1]){
-                 
-                count++;
-                sum +=count;
-                peak = count;
-                down = 0;
-
-            }else if(ratings[i]<ratings[i-1]){
+        int down = 0;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                count++; // Increment candy count
+                sum += count; // Add to total
+                peak = count; // Increment peak (same as peak = count)
+                down = 0; // Reset downward slope
+            } else if (ratings[i] < ratings[i - 1]) {
 
                 down++;
-                sum +=down;
+                sum += down;
                 count = 1;
                 if (down >= peak) {
                     sum++; // Because peak should be 1 greater than the max 
                 }
-            }else{
-                
+            } else {
+
                 down = 0;
                 count = 1;
                 peak = 1;
-                sum+=count;
+                sum += count;
             }
             // peak = Math.max(peak, count);
-            
-            
+
         }
         return sum;
-        
+
     }
 }
-
