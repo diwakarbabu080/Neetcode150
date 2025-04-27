454f46fd855175e8730e27b8da6431e2c89dc6c6
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-04-27 23:25:26 +0530

    Time: 3 ms (84.59%) | Memory: 45.7 MB (70.48%) - LeetSync

--- Strviers150SheetDSA/135-candy/candy.java
+++ Strviers150SheetDSA/135-candy/candy.java
@@ -2,34 +2,38 @@ class Solution {
     public int candy(int[] ratings) {
 
         int n = ratings.length;
-        int left[] = new int[n];
 
-        int right[] = new int[n];
-        int ans [] = new int[n];
-
-        left[0] = right[n-1] = 1;
-
-        
+        int sum = 1;
+        int peak = 1; 
+        int count = 1;
+        int down = 0;       
         for(int i = 1;i<n; i++){
-            if(ratings[i]>ratings[i-1]){
-                left[i] = left[i-1]+1;
-            }else{
-                left[i] = 1;
-            }
-        }
-        int sum =  Math.max(1, left[n-1]);
-        int curr = Math.max(1, left[n-1]);
-         for(int i = n-2;i>=0; i--){
-            if(ratings[i]>ratings[i+1]){
-                curr = curr+1;
+            if( ratings[i]>ratings[i-1]){
+                 
+                count++;
+                sum +=count;
+                peak = count;
+                down = 0;
 
+            }else if(ratings[i]<ratings[i-1]){
+
+                down++;
+                sum +=down;
+                count = 1;
+                if (down >= peak) {
+                    sum++; // Because peak should be 1 greater than the max 
+                }
             }else{
-                // right[i] = 1;
-                curr = 1;
+                
+                down = 0;
+                count = 1;
+                peak = 1;
+                sum+=count;
             }
-            sum += Math.max(left[i],curr );
+            // peak = Math.max(peak, count);
+            
+            
         }
-        
         return sum;
         
     }
