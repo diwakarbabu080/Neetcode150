5637b6c0e025c7cc97cd004a8837493bc7947cff
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-04-28 13:01:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (82.74%) - LeetSync

---
+++ Strviers150SheetDSA/957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+
+        int pos = 0;
+        int neg = 0;
+        // int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                // count = index;
+
+                pos++;
+                // index++;
+            } else {
+                // index--;
+                if (pos > 0) {
+                    pos--; // match with a previous '('
+                } else {
+                    neg++; // no '(' available to match, so unmatched ')'
+                }
+
+            }
+        }
+        return Math.abs(pos+neg);
+    }
+}
