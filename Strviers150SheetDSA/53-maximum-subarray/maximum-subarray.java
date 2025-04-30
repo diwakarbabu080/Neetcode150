bb1da38c2ccf4304fd0b1deb7c6107cf61928e64
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-04-30 21:17:57 +0530

    Time: 1 ms (99.58%) | Memory: 57 MB (70.55%) - LeetSync

--- Strviers150SheetDSA/53-maximum-subarray/maximum-subarray.java
+++ Strviers150SheetDSA/53-maximum-subarray/maximum-subarray.java
@@ -1,39 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-
-        // int sum = 0;
-        // int maxSum = Integer.MIN_VALUE;
-       
-        // int left = 0;
-        // int right = 0;
-        // int n = nums.length;
-
-
-        // while(right<n){
-
-        //     if(sum+nums[right]< nums[right]){
-        //         sum = nums[right];
-               
-        //     }else{
-        //         sum += nums[right];
-        //     }
-           
-        //     maxSum = Math.max(sum, maxSum);
-
-        //     right++;
-        // }
-         
-        // return maxSum;
-
-          int maxSum = nums[0]; // Initialize with first element
-        int currentSum = nums[0];
         
-        for (int i = 1; i < nums.length; i++) {
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-            maxSum = Math.max(maxSum, currentSum);
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i: nums){
+            if(i+sum<0){
+                sum = i;
+            }else{
+                sum = Math.max(sum+i, i);
+            }
+            max = Math.max(max, sum);
         }
-        
-        return maxSum;
+        return max;
+
     }
-    
 }
