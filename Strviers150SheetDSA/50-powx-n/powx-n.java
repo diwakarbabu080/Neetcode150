9a4075b88c47bc5ce234839758edafdde4bdfa36
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-05-02 06:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (88.17%) - LeetSync

--- Strviers150SheetDSA/50-powx-n/powx-n.java
+++ Strviers150SheetDSA/50-powx-n/powx-n.java
@@ -1,21 +1,26 @@
 class Solution {
     public double myPow(double x, int n) {
-        
+
+        // reson why we are using long is because if we just convert int min to max it will overflow that is reason we are using long.
+
         long power = n;
-        if(power<0){
-            x = 1/x;
+        if (power < 0) {
+            x = 1 / x;
             power = -power;
         }
 
         double result = 1;
-        while(power>0){
-            if((power&1) == 1){
+        while (power > 0) {
+            if ((power & 1) == 1) {
                 result *= x;
+                power = power - 1;
+            } else {
+                x = x * x;
+                power = power / 2;
             }
-            x = x*x;
-            power = power/2;
+
         }
         return result;
-        
+
     }
 }
