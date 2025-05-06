623479551c5fc49f13628ff6fa6a2926a33b4fc5
diwakarbabu080 <81071307+diwakarbabu080@users.noreply.github.com>
2025-05-07 00:30:40 +0530

    Time: 1 ms (99.93%) | Memory: 43.5 MB (61.43%) - LeetSync

--- Strviers150SheetDSA/90-subsets-ii/subsets-ii.java
+++ Strviers150SheetDSA/90-subsets-ii/subsets-ii.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        subsetsWithDupHelper(nums, ans, 0, new ArrayList<>());
-        return ans;
-
+        Arrays.sort(nums); // Important to handle duplicates
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), result);
+        return result;
     }
 
-    public void subsetsWithDupHelper(int[] nums, List<List<Integer>> ans, int index,
-            List<Integer> temp) {
-
-        ans.add(new ArrayList<>(temp));
-        for (int i = index; i < nums.length; i++) {
-
-            if(i>index && nums[i] == nums[i-1])continue;
-
-            temp.add(nums[i]);
-            subsetsWithDupHelper(nums, ans, i+1, temp);
-            temp.remove(temp.size()-1);
+    private void backtrack(int[] nums, int index, List<Integer> temp, List<List<Integer>> result) {
+        if (index == nums.length) {
+            result.add(new ArrayList<>(temp));
+            return;
         }
-         
 
+        // 1. Include current element
+        temp.add(nums[index]);
+        backtrack(nums, index + 1, temp, result);
+        temp.remove(temp.size() - 1);
+
+        // 2. Skip current element AND all its duplicates
+        int next = index + 1;
+        while (next < nums.length && nums[next] == nums[index]) {
+            next++;
+        }
+        backtrack(nums, next, temp, result);
     }
 }
